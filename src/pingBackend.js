248ddaad11a4e8bcb896fabe62513b66d26d4059
Ananddivine <109467220+Ananddivine@users.noreply.github.com>
2024-07-10 00:06:42 +0530

    Initial commit

--- src/pingBackend.js
+++ src/pingBackend.js
@@ -1,16 +1,22 @@
-// api/ping.js
+import axios from 'axios';
 
-export default function handler(req, res) {
+const pingBackend = () => {
   const backendUrl = 'https://backend-1-la1d.onrender.com';
-
-  fetch(`${backendUrl}/ping`)
-    .then(response => response.json())
-    .then(data => {
-      console.log('Ping successful:', data);
-      res.status(200).send('Ping successful');
+  axios.get(`${backendUrl}/ping`)
+    .then(response => {
+      if (response && response.status === 200) {
+        console.log('Ping successful:', response.data);
+        // Handle the response data here
+      } else {
+        console.error('Invalid response:', response);
+        // Handle unexpected responses or errors
+      }
     })
     .catch(error => {
       console.error('Ping failed:', error);
-      res.status(500).send('Ping failed');
+      // Handle errors here
     });
-}
+};
+
+
+export default pingBackend;
